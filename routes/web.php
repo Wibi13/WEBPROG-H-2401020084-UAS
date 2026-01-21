3bdc11cc754ba63516f2e57c84455e21c5e3a068
Wibi <dhenatawibi@gmail.com>
2026-01-21 19:49:03 +0800

    first commit

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AbsensiController;
-
-Route::get('/', [AbsensiController::class, 'index']);
-Route::post('/check-in', [AbsensiController::class, 'checkIn']);
-Route::post('/check-out', [AbsensiController::class, 'checkOut']);
-Route::post('/absensi/reset', [AbsensiController::class, 'resetToday']);
